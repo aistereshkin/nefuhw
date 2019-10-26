2c68e3ba6ada7a7daa0a4bc2e55df23580d1af85
aistereshkin <54761549+aistereshkin@users.noreply.github.com>
2019-10-26 22:34:03 +0900

    Update HW1.py

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,3 +1,4 @@
+n = int(input("Enter a number = "))
 def get_digit_sum(n):
     string_repr = str(abs(n))
     digits_sum = 0
@@ -6,4 +7,4 @@ def get_digit_sum(n):
     return digits_sum
 
 if __name__ == "__main__":
-    print (get_digit_sum(1000))
+    print (get_digit_sum(n))
